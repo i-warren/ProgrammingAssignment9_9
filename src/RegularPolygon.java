8b55a1b160ac2c56a2dc52a534485f0e727aa67d
Isaac Warren <isaacwarren25@gmail.com>
2024-01-25 12:40:17 -0500

    Pushing correct files.

--- src/RegularPolygon.java
+++ src/RegularPolygon.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class RegularPolygon {
+public class RegularPolygon {
+
+    private int n = 3;              // num sides
+    private double side = 1;        // length of side
+    private double x = 0;           // x-coordinate
+    private double y = 0;           // y-coordinate
+
+
+    // construct a regular polygon
+    RegularPolygon() {
+
+    }
+
+    RegularPolygon(int numSides, double lengthSide) {
+        n = numSides;
+        side = lengthSide;
+    }
+
+    RegularPolygon(int numSides, double lengthSide, double xPosition, double yPosition) {
+        n = numSides;
+        side = lengthSide;
+        x = xPosition;
+        y = yPosition;
+    }
+
+    //  number of sides
+    public int getNumberOfSides(){
+        return n;
+    }
+
+    // return length of side
+    public double getLengthOfSide() {
+        return side;
+    }
+
+    // return x-position
+    public double getX() {
+        return x;
+    }
+
+    // return y-position
+    public double getY() {
+        return y;
+    }
+
+    // set number of sides
+    public void setNumberOfSides(int numberOfSides){
+        n = numberOfSides;
+    }
+
+    // set length of side
+    public void setLengthOfSide(double lengthOfSide) {
+        side = lengthOfSide;
+    }
+
+    // set x-position
+    public void setXPos(double xPosition) {
+        x = xPosition;
+    }
+
+    // set y-position
+    public void setYPos(double yPosition) {
+        y = yPosition;
+    }
+
+    // return perimeter or regular polygon
+    public double getPerimeter() {
+        return side * n;
+    }
+
+    // return area of regular polygon
+    public double getArea() {
+        return (n * side * side) / (4 * (Math.tan(Math.PI / n)));
+    }
+
+    public String toString() {
+        return "Number of Sides: " + n + "\n" +
+                "Length of Sides: " + side + "\n" +
+                "Perimeter: " + this.getPerimeter() + "\n" +
+                "Area: " + this.getArea() + "\n" +
+                "Coordinates: " + this.getX() + "," +
+                this.getY() + "\n";
+    }
 }
--- src/RegularPolygonTest.java
+++ src/RegularPolygonTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class RegularPolygonTest {
+public class RegularPolygonTest {
+    public static void main(String[] args) {
+
+
+        // Construct a regular polygon using default constructor
+        RegularPolygon polygon1 = new RegularPolygon();
+
+        // Print statements about Polygon 1
+        System.out.println("Polygon 1");
+        System.out.println(polygon1);
+
+        // Construct a regular polygon using two arguments
+        RegularPolygon polygon2 = new RegularPolygon(6, 4);
+
+        // Print statements about Polygon 2 using toString
+        System.out.println("Polygon 2");
+        System.out.println(polygon2);
+
+        // Construct a regular polygon using four arguments
+        RegularPolygon polygon3 = new RegularPolygon(10, 4, 5.6, 7.8);
+
+        // Print statements about Polygon 3
+        System.out.println("Polygon 3");
+        System.out.println(polygon3);
+
+        // Test set methods on Polygon 3
+        System.out.println("Changing Polygon 3 \n");
+        polygon3.setNumberOfSides(12);
+        polygon3.setLengthOfSide(5.5);
+        polygon3.setXPos(9.9);
+        polygon3.setYPos(42.0);
+        System.out.println(polygon3);
+
+
+    }
 }
